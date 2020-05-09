83577df1adc0f5044edf9eb0d7af177feba6dfb1
IQWiksPell <stepan991220@gmail.com>
2020-05-09 14:15:31 +0300

    Base models for keys

--- keymanager/views.py
+++ keymanager/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
  
  
 def keymanager(request):
-    return render(request, "keymanager/index.html")
+    return render(request, "keymanager/index.html", {'username': auth.get_user(request).username})
